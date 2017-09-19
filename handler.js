ef9a4611e00a72bb5e8bda247f54d5c984684565
Pablo Osinaga <paguos@gmail.com>
2017-09-19 17:47:11 +0200

    Formated handler.js

--- handler.js
+++ handler.js
@@ -1,11 +1,11 @@
 'use strict';
 
-  module.exports.hello = (event, context, callback) =>
-       {
-           const response =
-           {
-statusCode: 200, body: JSON.stringify({
-                     message: `Hello, ${event.name}!`
-}) };
-           callback(null, response);
-       };
+module.exports.hello = (event, context, callback) => {
+    const response =
+        {
+            statusCode: 200, body: JSON.stringify({
+                message: `Hello, ${event.name}!`
+            })
+        };
+    callback(null, response);
+};
