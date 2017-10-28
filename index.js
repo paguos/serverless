784986c2101e74349d3a9b84a0b346678981af3e
Pablo Osinaga <pabloosinaga@wlan-141-23-199-10.tubit.tu-berlin.de>
2017-10-28 15:24:43 +0200

    Small Changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
 s3.listBuckets((err, data) => {
-  if (err) console.log(err, err.stack); // an error occurred
+  if (err) console.log(err, err.stack); // an error occurred!
   else console.log(data.Buckets); // successful response
 });
